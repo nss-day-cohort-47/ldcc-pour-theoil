0abde827652ff5ff78301292ee6ca2b161e745aa
Logan Demmy <frank.demmy@gmail.com>
2021-03-29 16:34:46 -0500

    added functionality to the add form

--- scripts/snacks/toppings.js
+++ scripts/snacks/toppings.js
@@ -1,7 +1,7 @@
 export const addToppings = (id) => {
     return `
     <li class="nav-item ms-1">
-        <select id="snackToppings__${id}" class="form-select form-select btn-info" aria-label="Select A Topping">
+        <select name="snackToppings__${id}" id="snackToppings__${id}" class="form-select form-select btn-info" aria-label="Select A Topping">
         <option selected>Select A Topping</option>
         <option value="1">One</option>
         <option value="2">Two</option>
